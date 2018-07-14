992d65a766d28767d63e3ffac33b3280335dcbab
Augusto Calaca <augusto@Calaca.Augusto>
2018-07-14 19:00:08 -0300

    Add uri2095.cpp

---
+++ uri/uri2095.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+
+#define pb push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+  FAST;
+
+
+  int s, n, q[100010];
+  multiset<int> noglo;
+  cin >> s;
+  for(int i = 0; i < s; i++) cin >> q[i];
+  for(int i = 0; i < s; i++) { cin >> n; noglo.insert(n); }
+
+  int ans = 0;
+  for(int i = 0; i < s; i++) {
+    auto fnd = noglo.upper_bound(q[i]);
+    if(fnd != noglo.end()) {
+      noglo.erase(fnd);
+      ans++;
+    }
+  }
+
+  cout << ans << "\n";
+  return 0;
+}
