90a14382e6fa91c808e1e846f17a54cbb0664fa0
Augusto Calaca <augusto@Calaca.Augusto>
2018-07-20 19:37:50 -0300

    Modified uri1570.cpp

--- uri/uri1570.cpp
+++ uri/uri1570.cpp
@@ -6,7 +6,7 @@
 #include <set>
 
 #define sq(v) (v) * (v)
-#define LIM (ll)floor(sqrt((1LL << 31) - 1))
+#define LIM (ll)floor(sqrt((1LL << 33) - 1))
 #define f first
 #define s second
 #define pb push_back
@@ -18,7 +18,7 @@ typedef unsigned long long ull;
 
 ll ans1, ans2;
 
-set<ll> st; // squares
+unordered_map<ll, int> st; // squares
 vector<ll> primes;
 ll marked[LIM + 10];
 
@@ -29,25 +29,24 @@ void sieve() {
       for(ll j = i * i; j <= LIM; j += i)
         marked[j] = false;
 
-  st.insert(0);
+  st[0] = 1;
   for(ll i = 2; i <= LIM; i++) {
-    st.insert(sq(i -1));
+    st[sq(i - 1)] = 1;
     if(marked[i])
       primes.pb(i);
   }
 }
 
 bool sumsquare(ll n) {
-  ll j = 0;
   ll m = n;
-  for(ll i = 0; primes[i] * primes[i] <= n || j * j <= m; i++, j++) {
+  for(ll i = 0, j = 0; primes[i] * primes[i] <= n && j * j <= m; i++, j++) {
     int c = 0;
 
     for(; n % primes[i] == 0; n /= primes[i], c++);
     if(primes[i] % 4 == 3 && c & 1) return false;
 
     ll sqr = m - j * j;
-    if(st.find(sqr) != st.end()) {
+    if(st[sqr] != 0) {
       ans1 = j;
       ans2 = floor(sqrt(sqr));
       return true;
@@ -61,13 +60,13 @@ int main() {
   FAST;
 
   sieve();
-  int n, p, q, k = 1;
+  ll n, p, q, k = 1;
 
   cin >> n;
   while(n--) {
     cin >> p >> q;
     cout << "Case " << k++ << ":\n";
-    if(sumsquare(p + q))
+    if(sumsquare(abs(p) + abs(q)))
       cout << ans1 << " " << ans2 << "\n";
     else
       cout << "Impossible.\n";
