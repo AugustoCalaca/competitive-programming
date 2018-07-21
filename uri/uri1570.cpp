117e6f72c7532a9bf3adce0d205f111c94159a02
Augusto Calaca <augusto@Calaca.Augusto>
2018-07-21 19:06:46 -0300

    Modified uri1570.cpp more one time

--- uri/uri1570.cpp
+++ uri/uri1570.cpp
@@ -2,11 +2,12 @@
 #include <algorithm>
 #include <cmath>
 #include <vector>
-#include <unordered_map>
+#include <map>
 #include <set>
 
+#define eps -(1e6)
 #define sq(v) (v) * (v)
-#define LIM (ll)floor(sqrt((1LL << 33) - 1))
+#define LIM (ll)floor(sqrt((1LL << 32) - 1))
 #define f first
 #define s second
 #define pb push_back
@@ -16,58 +17,93 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-ll ans1, ans2;
+ll ans1, ans2, p, q, b1, b2;
 
-unordered_map<ll, int> st; // squares
-vector<ll> primes;
-ll marked[LIM + 10];
-
-void sieve() {
-  fill(marked, marked + LIM, true);
-  for(ll i = 2; i * i <= LIM; i++)
-    if(marked[i])
-      for(ll j = i * i; j <= LIM; j += i)
-        marked[j] = false;
-
-  st[0] = 1;
-  for(ll i = 2; i <= LIM; i++) {
-    st[sq(i - 1)] = 1;
-    if(marked[i])
-      primes.pb(i);
-  }
+bool checkx(ll x) {
+  return 2 * sq(x) * sq(x) + sq(q) - sq(x) * b1 == 0;
 }
 
-bool sumsquare(ll n) {
-  ll m = n;
-  for(ll i = 0, j = 0; primes[i] * primes[i] <= n && j * j <= m; i++, j++) {
-    int c = 0;
+bool checky(ll y) {
+  return 2 * sq(y) * sq(y) + sq(p) - sq(y) * b2 == 0;
+}
 
-    for(; n % primes[i] == 0; n /= primes[i], c++);
-    if(primes[i] % 4 == 3 && c & 1) return false;
+bool solvex() {
+  b1 = 3 * q + p;
+  double delta = sq(b1) - 8 * sq(q);
+  if(delta < 0.0) return false;
 
-    ll sqr = m - j * j;
-    if(st[sqr] != 0) {
-      ans1 = j;
-      ans2 = floor(sqrt(sqr));
-      return true;
-    }
+  delta = sqrt(delta);
+
+  double z1 = (b1 - delta) / 4;
+  double z2 = (b1 + delta) / 4;
+
+  z1 = floor(sqrt(z1));
+  z2 = floor(sqrt(z2));
+
+  if(checkx(z1)) {
+    ans1 = z1;
+    return true;
   }
+  else if(checkx(z1 + 1)) {
+    ans1 = z1 + 1;
+    return true;
+  }
+  else if(checkx(z2)) {
+    ans1 = z2;
+    return true;
+  }
+  else if(checkx(z2 + 1)) {
+    ans1 = z2 + 1;
+    return true;
+  }
+  else return false;
+}
 
-  return (n % 4 == 3) ? false : true;
+bool solvey() {
+  b2 = 3 * p + q;
+  double delta = sq(b2) - 8 * sq(p);
+  if(delta < 0.0) return false;
+
+  delta = sqrt(delta);
+
+  double z1 = (b2 - delta) / 4;
+  double z2 = (b2 + delta) / 4;
+
+  z1 = floor(sqrt(z1));
+  z2 = floor(sqrt(z2));
+
+  if(checky(z1)) {
+    ans2 = z1;
+    return true;
+  }
+  else if(checky(z1 + 1)) {
+    ans2 = z1 + 1;
+    return true;
+  }
+  else if(checky(z2)) {
+    ans2 = z2;
+    return true;
+  }
+  else if(checky(z2 + 1)) {
+    ans2 = z2 + 1;
+    return true;
+  }
+  else return false;
 }
 
 int main() {
   FAST;
 
-  sieve();
-  ll n, p, q, k = 1;
+  ll n, k = 1;
 
   cin >> n;
   while(n--) {
     cin >> p >> q;
     cout << "Case " << k++ << ":\n";
-    if(sumsquare(abs(p) + abs(q)))
+    if(solvex() && solvey()) {
+      if(p * q < 0) ans1 = -ans1, ans2 = -ans2;
       cout << ans1 << " " << ans2 << "\n";
+    }
     else
       cout << "Impossible.\n";
   }
